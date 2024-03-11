930cd000cdc127762a6dda8dd64799ecbe682899
keringKirwa <kkirwa230@gmail.com>
2024-03-11 14:20:55 +0300

    first commit

---
+++ src/main/java/org/tps/hadoop/HDFS_WordCounter_Runner.java
@@ -0,0 +1,29 @@
+package org.tps.hadoop;
+
+import java.io.IOException;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.TextOutputFormat;
+
+public class HDFS_WordCounter_Runner {
+    public static void main(String[] args) throws IOException{
+        JobConf conf = new JobConf(HDFS_WordCounter_Runner.class);
+        conf.setJobName("WordCount");
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
+        conf.setMapperClass(HDFS_WordCounter_Mapper.class);
+        conf.setCombinerClass(HDF_WordCounter_Reducer.class);
+        conf.setReducerClass(HDF_WordCounter_Reducer.class);
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
+        FileInputFormat.setInputPaths(conf,new Path(args[0]));
+        FileOutputFormat.setOutputPath(conf,new Path(args[1]));
+        JobClient.runJob(conf);
+    }
+}
